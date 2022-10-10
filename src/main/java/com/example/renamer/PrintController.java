4058a0690bb02c2c42ff9ce9bf02f4c0282422d7
Balázs Hermann <hermann.balaazs@gmail.com>
2022-10-10 14:41:09 +0200

    upgraded the directory selection and the extension picker combobox

--- src/main/java/com/example/renamer/PrintController.java
+++ src/main/java/com/example/renamer/PrintController.java
@@ -42,7 +42,6 @@ public class PrintController {
     public void initialize() {
         displayLabel.setText("Enter the path: ");
         chooseDirButton.setText("Select files");
-
     }
 
     @FXML
